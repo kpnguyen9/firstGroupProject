a327866e8d9a92b8d157c0ad79284c58cf3df427
kpnguyen9 <kpnguyen2@gmail.com>
2022-01-20 11:34:17 -0600

    added script to print weather

--- script.js
+++ script.js
@@ -55,7 +55,6 @@ submitButton.addEventListener("click", () => {
   parkList.innerHTML = "";
 
   let parkName = parkInput.value;
-
   let parkURL = `https://developer.nps.gov/api/v1/parks?q=${parkName}&api_key=${campKey}`;
 
   fetch(parkURL)
@@ -65,8 +64,6 @@ submitButton.addEventListener("click", () => {
     .catch(reportError)
     .then(printParkName);
 });
-
-//next...extract the parkCode and fetch campground based off parkCode
 
 const printCampsites = (camps) => {
   const campContainer = document.getElementById("campContainer");
@@ -81,6 +78,32 @@ const printCampsites = (camps) => {
     campContainer.appendChild(createLi);
   });
 };
+
+const extractCurrentWeather = (data) => {
+  console.dir(data.current);
+  return data.current;
+};
+
+const printCurrentWeather = (current) => {
+  const weatherContainer = document.getElementById("weatherContainer");
+
+  const createImg = document.createElement("img");
+  createImg.setAttribute = ("src", `${current.condition.icon}`);
+  weatherContainer.appendChild(createImg);
+
+  let currentWeather = `<ul>
+  <li>Today's weather was last updated ${current.last_updated}</li>
+  <li>Condition: ${current.condition.text}</li>
+  <li>Temperature: ${current.temp_f}F</li>
+  <li>Wind Speed: ${current.wind_mph}mph</li>
+  <li>Precipitation(inches): ${current.precip_in}</li>
+  <li>Humidity: ${current.humidity}%</li>
+  </ul>`;
+
+  weatherContainer.innerHTML = currentWeather;
+};
+//extract the parkCode and fetch campground based off parkCode
+//print campsites and weather forecast after clicking park button
 
 parkInfo.addEventListener("click", (event) => {
   // console.log("parkInfo div was clicked");
@@ -99,8 +122,8 @@ parkInfo.addEventListener("click", (event) => {
 
     let weatherForecastURL = `http://api.weatherapi.com/v1/forecast.json?key=${weatherKey}&q=${latLong}&days=7&aqi=no&alerts=no`;
 
-    console.log(weatherCurrentURL);
-    console.log(weatherForecastURL);
+    // console.log(weatherCurrentURL);
+    // console.log(weatherForecastURL);
 
     fetch(parkURL)
       .then((response) => response.json())
@@ -110,8 +133,37 @@ parkInfo.addEventListener("click", (event) => {
 
     fetch(weatherForecastURL)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then(extractCurrentWeather)
+      .then(printCurrentWeather);
   }
 });
 
-//also...extract latLong and fetch weather forecast based off latLong
+// {
+//   "last_updated_epoch": 1642697100,
+//   "last_updated": "2022-01-20 10:45",
+//   "temp_c": -0.5,
+//   "temp_f": 31.1,
+//   "is_day": 1,
+//   "condition": {
+//       "text": "Sunny",
+//       "icon": "//cdn.weatherapi.com/weather/64x64/day/113.png",
+//       "code": 1000
+//   },
+//   "wind_mph": 12.5,
+//   "wind_kph": 20.2,
+//   "wind_degree": 31,
+//   "wind_dir": "NNE",
+//   "pressure_mb": 1031,
+//   "pressure_in": 30.45,
+//   "precip_mm": 0,
+//   "precip_in": 0,
+//   "humidity": 40,
+//   "cloud": 18,
+//   "feelslike_c": -5.9,
+//   "feelslike_f": 21.4,
+//   "vis_km": 10,
+//   "vis_miles": 6,
+//   "uv": 2,
+//   "gust_mph": 17,
+//   "gust_kph": 27.4
+// }
